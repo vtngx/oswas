f11f6f8812b1471d95f2c3a3e1848426c0c537e3
Viet Nguyen <vieth.nguyen2505@gmail.com>
2021-12-01 21:07:28 +0700

    fix proxy script format

--- app/lib/save_respone.py
+++ app/lib/save_respone.py
@@ -13,21 +13,14 @@ from mitmproxy.net.http.http1 import assemble_request
 current_path = os.getcwd()
 
 def request(flow):
-    print('='*80)
     os.chdir(current_path)
     directory = Path(urlparse(flow.request.pretty_url).scheme + "\\\\" + urlparse(flow.request.pretty_url).netloc)
     directory.mkdir(exist_ok=True)
     os.chdir(f'./{directory}')
-    print(os.getcwd())
-    print('URL: '+flow.request.pretty_url)
 
     #create file .txt with random id
     name = f'request-{uuid.uuid4()}.txt'
     request_data = flow.request
-    print('/n')
-    print('*'*80)
-    print(request_data)
-    print('*'*80)
 
     #write decode request into file for later analysis
     with open(name, 'w') as f:
